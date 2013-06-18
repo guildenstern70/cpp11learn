8cf87030edc93398841e29337d6e5f7335d25f47
Alessio Saltarin <alessio@Lucifero>
2013-06-18 14:28:19 +0200

    Initial version

---
+++ Cpp11Learn/src/main.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	auto i = 43;
+	cout << "The answer is " << i << endl; // prints !!!Hello World!!!
+	return 0;
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-cpp11learn
-==========
-
-A collection of simple source code examples in C++ 11, featuring key aspects of the language.
